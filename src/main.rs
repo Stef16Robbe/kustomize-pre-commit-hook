2f6ab4b4fe23d3997cdec5f5b5483a04623775bd
Stef Robbe <stef.robbe@protonmail.com>
2024-07-30 15:53:24 +0200

    perf: use tokio tasks for async execution

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,45 @@ use std::path::{Path, PathBuf};
 use std::process::Command;
 use std::str;
 
-fn build(path: &Path, cwd: &PathBuf) {
+#[tokio::main]
+async fn main() {
+    let cwd = env::current_dir().unwrap();
+
+    let args: Vec<String> = env::args().collect();
+
+    let kustomize_paths: Vec<PathBuf> = args
+        .iter()
+        .skip(1) // thanks to pre-commit we can assume we get at least one `kustomization.yaml` file here
+        .map(|p| Path::new(p).to_path_buf())
+        .collect();
+
+    let mut handles = Vec::new();
+
+    for path in kustomize_paths {
+        println!("Building: {}...", path.display());
+
+        let path = path.clone();
+        let cwd = cwd.clone();
+        let handle = tokio::spawn(async move {
+            let path = get_path_parent(&path).await;
+            build(path, &cwd).await;
+        });
+
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        let _ = handle.await;
+    }
+}
+
+async fn get_path_parent(path: &Path) -> &Path {
+    path.parent()
+        .filter(|p| !p.as_os_str().is_empty())
+        .unwrap_or_else(|| Path::new("."))
+}
+
+async fn build(path: &Path, cwd: &PathBuf) {
     let res = Command::new("kustomize")
         .current_dir(cwd)
         .arg("build")
@@ -19,31 +57,6 @@ fn build(path: &Path, cwd: &PathBuf) {
         );
     } else {
         // println!("{:?}", str::from_utf8(&res.stdout).unwrap());
-        println!("Build success! Your code works B)");
-    }
-}
-
-fn get_path_parent(path: &Path) -> &Path {
-    path.parent()
-        .filter(|p| !p.as_os_str().is_empty())
-        .unwrap_or_else(|| Path::new("."))
-}
-
-fn main() {
-    let cwd = env::current_dir().unwrap();
-
-    let args: Vec<String> = env::args().collect();
-
-    let kustomize_paths: Vec<PathBuf> = args
-        .iter()
-        .skip(1) // thanks to pre-commit we can assume we get at least one `kustomization.yaml` file here
-        .map(|p| Path::new(p).to_path_buf())
-        .collect();
-
-    for path in kustomize_paths {
-        println!("Building: {}...", path.display());
-
-        let path = get_path_parent(&path);
-        build(path, &cwd);
+        println!("Build {} success!", path.display());
     }
 }
